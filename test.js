2bec60f608ff5bc920ec7128257f1c383b9f7376
Scott Richmond <kredati@users.noreply.github.com>
2023-11-26 17:28:15 -0500

    Mostly everything!

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 import {parser} from "./parser.js"
 
-const source = "nil; true; false; 12; \"foo\""
+const source = `
+if foo
+then do abcd
+> xyz
+> 
+else asdf
+`
 
 const parse = (source) => parser.parse(source.concat("\n"))
 
